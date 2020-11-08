22a5dcc91cc0f98ac9ff5d6e3c16b28e20bec330
Martín Stancanelli <mastanca14@gmail.com>
2020-11-08 15:35:27 -0300

    map routes

---
+++ cmd/web/handlers/get_balance_handler.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mastanca/accounting-notebook-be/internal/usecases"
+)
+
+type GetBalanceHandler interface {
+	Handle(c *gin.Context)
+}
+
+type getBalanceHandlerImpl struct {
+	getAccount usecases.GetAccount
+}
+
+func (g getBalanceHandlerImpl) Handle(c *gin.Context) {
+	account, err := g.getAccount.Execute(c)
+	if err != nil {
+		_ = c.AbortWithError(http.StatusInternalServerError, err)
+	}
+	if account == nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "nonexistent account"})
+	}
+	c.JSON(http.StatusOK, gin.H{"balance": account.GetBalance()})
+}
+
+func NewGetBalanceHandlerImpl(getAccount usecases.GetAccount) *getBalanceHandlerImpl {
+	return &getBalanceHandlerImpl{getAccount: getAccount}
+}
+
+var _ GetBalanceHandler = (*getBalanceHandlerImpl)(nil)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,17 +3,36 @@ package main
 import (
 	"log"
 
+	"github.com/mastanca/accounting-notebook-be/internal/domain/account"
+	"github.com/mastanca/accounting-notebook-be/internal/usecases"
+
 	"github.com/mastanca/accounting-notebook-be/cmd/web/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	accountRepository := account.NewInMemoryRepositoryImpl()
+
+	getAccount := usecases.NewGetAccountImpl(accountRepository)
+	commitTransaction := usecases.NewCommitTransactionImpl(accountRepository)
+	getTransactionById := usecases.NewGetTransactionByIdImpl(accountRepository)
 
 	pingHandler := handlers.NewPingHandlerImpl()
+	getBalanceHandler := handlers.NewGetBalanceHandlerImpl(getAccount)
+	getTransactionsHandler := handlers.NewGetTransactionsHandlerImpl(getAccount)
+	commitTransactionsHandler := handlers.NewGetCommitTransactionHandlerImpl(commitTransaction)
+	getTransactionByIdHandler := handlers.NewGetTransactionByIdHandlerImpl(getTransactionById)
 
 	router := gin.Default()
+	router.GET("/", getBalanceHandler.Handle)
 	router.GET("/ping", pingHandler.Handle)
+	transactions := router.Group("/transactions")
+	{
+		transactions.GET("", getTransactionsHandler.Handle)
+		transactions.POST("", commitTransactionsHandler.Handle)
+		transactions.GET("/:id", getTransactionByIdHandler.Handle)
+	}
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal("error initializing server")
